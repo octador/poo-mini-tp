c84b21fa7d63944597b5160f32b0e9429c7312bb
octador <mutuon.j@gmail.com>
2024-01-15 18:54:02 +0100

    avanced party 2

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// include_once("./class/qcm.php");
+// include_once("./class/question.php");
+// include_once("./class/answer.php");
+
+// $A = new Answer('ProgrammatiOn Orientée Outil')
+
+?>
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quizz</title>
+</head>
+<body>
+    
+</body>
+</html> -->
